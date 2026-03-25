ae174fb84cc25b3e978a27d0f8e2538c16830fc4
Inaki Villar <inaki.seri@gmail.com>
2026-03-25 09:31:40 -0700

    Adding element

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,24 @@ pluginManagement {
     }
 }
 plugins {
+    id("com.gradle.develocity") version "4.3.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
     id("com.fueledbycaffeine.spotlight") version "1.4.1"
 }
+
+val isCI = providers.environmentVariable("CI").isPresent
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com"
+    buildScan {
+        uploadInBackground = !isCI
+        publishing.onlyIf { it.isAuthenticated }
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { "0.0.0.0" } }
+        }
+    }
+}
+
 rootProject.name = "projectMetrics2"
 
 dependencyResolutionManagement {
